eebe60c8b179270530714b998293d7fd95d20801
jooemrp <75409382+jooemrp@users.noreply.github.com>
2024-12-04 16:13:32 +0800

    update program page

--- modules/app/settings.js
+++ modules/app/settings.js
@@ -1,4 +1,4 @@
-$(document).on('ready', function() {
+$(document).on("ready", function () {
 	global_settings(defaultval);
 	//console.log(defaultval.theme);
 
@@ -11,118 +11,152 @@ $(document).on('ready', function() {
 	}
 
 	// on click any link apply settings
-	$(document).on('click', '.appsettings', function(e) {
+	$(document).on("click", ".appsettings", function (e) {
 		e.preventDefault();
 
-		var type = $(this).attr('data-type');
-		var value = $(this).attr('data-value');
+		var type = $(this).attr("data-type");
+		var value = $(this).attr("data-value");
 
 		// console.log(type+value);
 
-		$(".appsettings[data-type='" + type + "']").removeClass('active');
-		$(".appsettings[data-type='" + type + "'][data-value='" + value + "']").addClass('active');
+		$(".appsettings[data-type='" + type + "']").removeClass("active");
+		$(
+			".appsettings[data-type='" + type + "'][data-value='" + value + "']"
+		).addClass("active");
 
-		if (type === 'site_mode') {
+		if (type === "site_mode") {
 			// site mode related settings here.
-			if (value === 'dark') {
-				var set_sitemode = { header: 'dark', menu: 'dark', footer: 'dark', footer_menu_style: 'dark' };
+			if (value === "dark") {
+				var set_sitemode = {
+					header: "dark",
+					menu: "dark",
+					footer: "dark",
+					footer_menu_style: "dark",
+				};
 				site_mode_settings(set_sitemode);
-			} else if (value === 'light') {
-				var set_sitemode = { header: 'light', menu: 'light', footer: defaultval['footer'], footer_menu_style: 'light' };
+			} else if (value === "light") {
+				var set_sitemode = {
+					header: "light",
+					menu: "light",
+					footer: defaultval["footer"],
+					footer_menu_style: "light",
+				};
 				site_mode_settings(set_sitemode);
 			}
 		}
 
-		var attr = 'data-' + type + '';
-		$('body').attr(attr, value);
+		var attr = "data-" + type + "";
+		$("body").attr(attr, value);
 
 		//settings_session();
 		localStorage[type] = value;
 	});
 
 	// on click any link apply settings
-	$(document).on('click', '.nav-site-mode', function(e) {
+	$(document).on("click", ".nav-site-mode", function (e) {
 		e.preventDefault();
 		var active = $(".appsettings[data-type='site_mode'].active");
-		var mode = active.attr('data-value');
+		var mode = active.attr("data-value");
 		switch_site_mode(mode);
 		//settings_session();
 	});
 
 	// on click any link apply settings
-	$(document).on('click', '.switch-site-mode', function(e) {
+	$(document).on("click", ".switch-site-mode", function (e) {
 		e.preventDefault();
-		var mode = ''; 
-		if($(this).hasClass('light')){
+		var mode = "";
+		if ($(this).hasClass("light")) {
 			mode = "dark";
-		} else if($(this).hasClass('dark')){
+		} else if ($(this).hasClass("dark")) {
 			mode = "light";
-		} 
+		}
 		switch_site_mode(mode);
 		//settings_session();
 	});
 
-	function switch_site_mode(mode){
+	function switch_site_mode(mode) {
 		console.log(mode);
-		if (mode === 'light') {
-			var set_sitemode = { header: 'dark', menu: 'dark', footer: 'dark', footer_menu_style: 'dark', site_mode: 'dark' };
+		if (mode === "light") {
+			var set_sitemode = {
+				header: "dark",
+				menu: "dark",
+				footer: "dark",
+				footer_menu_style: "dark",
+				site_mode: "dark",
+			};
 			site_mode_settings(set_sitemode);
 		} else {
-			var set_sitemode = { header: 'light', menu: 'light', footer: defaultval['footer'], footer_menu_style: 'light', site_mode: 'light' };
+			var set_sitemode = {
+				header: "light",
+				menu: "light",
+				footer: defaultval["footer"],
+				footer_menu_style: "light",
+				site_mode: "light",
+			};
 			site_mode_settings(set_sitemode);
 		}
 	}
 
-
 	function site_mode_settings(obj) {
-		$.each(obj, function(type, value) {
+		$.each(obj, function (type, value) {
 			//console.log( type + ": " + value );
-			$(".appsettings[data-type='" + type + "']").removeClass('active');
-			$(".appsettings[data-type='" + type + "'][data-value='" + value + "']").addClass('active');
-			$('body').attr('data-' + type, value);
+			$(".appsettings[data-type='" + type + "']").removeClass("active");
+			$(
+				".appsettings[data-type='" +
+					type +
+					"'][data-value='" +
+					value +
+					"']"
+			).addClass("active");
+			$("body").attr("data-" + type, value);
 			localStorage[type] = value;
 		});
 	}
 
 	function global_settings(defaultval) {
-
 		// Set default values in localstorage if no value is set yet
 		for (var key in defaultval) {
 			if (defaultval.hasOwnProperty(key)) {
-
 				// console.log(key + ' : ' + defaultval[key]);
-				if (localStorage.getItem(key) === '' || localStorage.getItem(key) === null) {
+				if (
+					localStorage.getItem(key) === "" ||
+					localStorage.getItem(key) === null
+				) {
 					// console.log('set now: ');
 					localStorage[key] = defaultval[key];
 				}
-
 			}
 
 			//console_log(key+": "+localStorage[key]);
 			// Set body tag attributes based on localstorage
 			// Also Set Settings checked values based on localstorage
-			if (localStorage.getItem(key) != '') {
-				$('body').attr('data-' + key, localStorage[key]);
-				$(".appsettings[data-type='" + key + "']").removeClass('active');
-				$(".appsettings[data-type='" + key + "'][data-value='" + localStorage[key] + "']").addClass('active');
+			if (localStorage.getItem(key) != "") {
+				$("body").attr("data-" + key, localStorage[key]);
+				$(".appsettings[data-type='" + key + "']").removeClass(
+					"active"
+				);
+				$(
+					".appsettings[data-type='" +
+						key +
+						"'][data-value='" +
+						localStorage[key] +
+						"']"
+				).addClass("active");
 			}
 		}
-
 	}
-
 });
 //console.log(localStorage);
 // resetSettings();
 
-function resetSettings(){
+function resetSettings() {
 	console.log(localStorage);
 
 	for (var key in defaultval) {
 		if (defaultval.hasOwnProperty(key)) {
 			//console.log(key + ' : ' + defaultval[key]);
-				console.log('reset: '+key);
-				localStorage[key] = '';
+			console.log("reset: " + key);
+			localStorage[key] = "";
 		}
 	}
-
 }
